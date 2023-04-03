3f7e22a602c6d099323873151b44aaa4359a1c61
Danya <danyat02@yandex.ru>
2023-04-04 00:27:01 +0300

    Bug fixed

--- src/store/actions/userActions.ts
+++ src/store/actions/userActions.ts
@@ -51,11 +51,7 @@ export const login = (user) => {
                 withCredentials: true
             });
 
-            dispatch(userActionName(res.data.user.name))
-            dispatch(userActionSurname(res.data.user.surname))
-            dispatch(userActionEmail(res.data.user.email))
-            dispatch(userActionAge(res.data.user.date))
-            dispatch(userActionPhoto(res.data.user.photo))
+            dispatch(loadingProfile())
         } catch (error) {
             if (error.response.data.status === 401) {
                 return 401;
